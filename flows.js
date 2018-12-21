65a45909dd208b7447d66817ab4c439309f89b0c
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2018-12-21 13:17:52 -0800

    parse constructor

--- flows.js
+++ flows.js
@@ -7,14 +7,14 @@ const fromSignature = require('./jsonrpc/payload/from');
 const Payload = require('./jsonrpc/payload');
 const { showAllWrites, showConstructor } = require('./cli/abi');
 
-exports.entry = async (ask) => {
+exports.entry = async ask => {
   const action = await ask(
     chooseOneOf('actions', [
       'Transfer Ether',
       'Deploy a contract',
       'Make a method call to a deployed contract',
-      'Read a contract',
-    ]),
+      'Read a contract'
+    ])
   );
   if (false) {
   } else if (action === '1') {
@@ -28,69 +28,90 @@ exports.entry = async (ask) => {
   }
 };
 
-exports.transferEther = async (ask) => {
+exports.transferEther = async ask => {
   const to = await ask(
-    'Please type the address of the your receiptian, i.e. (0xFF0E3299e55EFD859176D582FC805481e8344915): ',
+    'Please type the address of the your receiptian, i.e. (0xFF0E3299e55EFD859176D582FC805481e8344915): '
   );
   const value = await ask(
-    'Please type the Ether value you would like to send in wei:\nExample: (10000000000) for 10 gwei',
+    'Please type the Ether value you would like to send in wei:\nExample: (10000000000) for 10 gwei'
   );
   const nonce = parseInt(await ask('Please type the nonce, i.e. (30):'), 10);
   const gasPrice = await ask(
-    'Please type the gas price in wei:\nExample: (10000000000) for 10 gwei',
+    'Please type the gas price in wei:\nExample: (10000000000) for 10 gwei'
   );
   console.log({ to, value, nonce, gasPrice });
   const rawTx = writeActions.transferEther(nonce, value, gasPrice, to);
   return exports.chooseHowToSign(ask, rawTx);
 };
 
-exports.deployContract = async (ask) => {
+const askOneParam = ask => {
+  return async param => {
+    return ask(
+      `Please type the constructor argument for "${param.name} (${
+        param.type
+      })":`
+    );
+  };
+};
+
+const async1By1 = async (f, items) => {
+  const go = async (f, all, items) => {
+    if (items.length === 0) {
+      return all;
+    }
+
+    const one = items.shift();
+    const value = await f(one);
+    all.push(value);
+    return go(f, all, items);
+  };
+  return go(f, [], items);
+};
+
+const askConstructors = async (ask, constructor) => {
+  const inputs = constructor.inputs;
+  // [string]
+  const all = await async1By1(askOneParam(ask), inputs);
+  return all;
+};
+
+exports.deployContract = async ask => {
   const abiPath = await ask(
-    'Please type the path to the abi json file, i.e. (../src/kitty-core/Offers.json):',
+    'Please type the path to the abi json file, i.e. (./abis/Offers.json):'
   );
   // TODO: it's vulnerable to load a json file with any path, better to add some check
   // but for simplicity, I'm allowing it for now.
   const abiJSON = require(abiPath);
   const constructor = showConstructor(abiJSON);
+  console.log('Parsed contract constructor:');
   console.log(constructor);
-  const params = [
-    '0x5328276603d169165d0f71ca67ccc89c45027df3',
-    '0xb8a3FD5785D700E485EC68A13EEC1E9670889cB5',
-    '86400',
-    '104500000000000000',
-    '1000',
-    '4000000000000000',
-    '450',
-  ];
-  // const params = (await ask(
-  //   'Please type the parameters to the contract constructor, separates multiple parameters with comma (","), i.e. ():',
-  // )).split(',');
+  const params = await askConstructors(ask, constructor);
   let value = '0';
   if (constructor.payable) {
     value = await ask(
-      'Please type the Ether value you would like to send to the contract constructor in wei:\nExample: (10000000000) for 10 gwei',
+      'Please type the Ether value you would like to send to the contract constructor in wei:\nExample: (10000000000) for 10 gwei'
     );
   }
   const nonce = parseInt(await ask('Please type the nonce, i.e. (30):'), 10);
   const gasPrice = await ask(
-    'Please type the gas price in wei:\nExample: (10000000000) for 10 gwei',
+    'Please type the gas price in wei:\nExample: (10000000000) for 10 gwei'
   );
   const gasLimit = await ask(
-    'Please type the gas limit in wei:\nExample: (10) for 10 gas',
+    'Please type the gas limit in wei:\nExample: (10) for 10 gas'
   );
   const txBase = makeTxBase(nonce, gasPrice, gasLimit, value);
   const rawTx = writeActions.deployContract(abiJSON, txBase, params);
   return exports.chooseHowToSign(ask, rawTx);
 };
 
-exports.writeContract = async (ask) => {
+exports.writeContract = async ask => {
   const contractName = await ask('contract (Offers): ');
   const method = await ask('method (setCOO): ');
   const params = await ask(
-    'params (0x57831a0c76ba6b4fdcbadd6cb48cb26e8fc15e93): ',
+    'params (0x57831a0c76ba6b4fdcbadd6cb48cb26e8fc15e93): '
   );
   const contractAddress = await ask(
-    'contractAddress (0x57831a0c76ba6b4fdcbadd6cb48cb26e8fc15e93): ',
+    'contractAddress (0x57831a0c76ba6b4fdcbadd6cb48cb26e8fc15e93): '
   );
   const value = await ask('value: ');
   const nonce = await ask('nonce: ');
@@ -103,7 +124,7 @@ exports.writeContract = async (ask) => {
     txBase,
     contractAddress,
     method,
-    params,
+    params
   );
   return exports.chooseHowToSign(ask, rawTx);
 };
@@ -116,8 +137,8 @@ exports.chooseHowToSign = async (ask, rawTx) => {
       'generates payload for dapper signing service to sign and send',
       'Provide private key to sign',
       'show the json transaction for your own wallet to sign',
-      'show the js code for signing with your metamask wallet',
-    ]),
+      'show the js code for signing with your metamask wallet'
+    ])
   );
 
   if (false) {
@@ -133,7 +154,7 @@ exports.chooseHowToSign = async (ask, rawTx) => {
 
 exports.signWithPrivateKey = async (ask, rawTx) => {
   const privateKey = await ask(
-    'Please type your private key\nExample: (0x197...)',
+    'Please type your private key\nExample: (0x197...)'
   );
   // hex0x
   const signedTx = signByKey.signWithPrivateKey(rawTx, privateKey);
